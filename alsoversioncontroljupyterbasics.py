344e30a7d7d1b16daacd38af6ab335d5b331058a
karolinehc <karoline_hc@hotmail.com>
2021-07-08 22:41:23 -0500

    init

---
+++ alsoversioncontroljupyterbasics.py
@@ -0,0 +1,21 @@
+# string variable
+firts_name = "karoline"
+print(firts_name)
+
+# an integer variable
+age = 39
+print(age)
+
+# a floating point variable
+weight = 45.5
+print(weight)
+
+# bool
+married = False
+print(married)
+
+# List
+hobbits = ["yoga", "watchingdocumentaries", "skate", "tech"]
+
+print(hobbits)
+#
